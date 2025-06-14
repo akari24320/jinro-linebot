dcea7a42b07986f55005fffbb0e739ba92100344
akari24320 <24.a.ito.nutfes@gmail.com>
2025-06-14 10:22:29 +0900

    ファイル整理

--- src/bot/lineClient.ts
+++
@@ -1,9 +0,0 @@
-import { messagingApi } from "@line/bot-sdk";
-import dotenv from "dotenv";
-dotenv.config();
-
-const client = new messagingApi.MessagingApiClient({
-  channelAccessToken: process.env.CHANNEL_ACCESS_TOKEN || "",
-});
-
-export default client;
